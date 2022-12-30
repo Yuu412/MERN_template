cb6e5102f465e64141c5b51e425613ce6b2435dc
Yuu412 <yuu.yoshi12@outlook.jp>
2022-12-30 15:27:21 +0900

    first commit

--- express-docker/server.js
+++ express-docker/server.js
@@ -1,34 +1,11 @@
-const express = require('express');
-const PORT = process.env.PORT || 5500;
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const cors = require('cors');
-app.use(express.json());
-app.use(cors());
+PORT = 5050;
 
-// Database connection
-mongoose.set("strictQuery", false);
-mongoose.connect('mongodb://yuu422:Nkbt7QR9EjEe%uk@mongo:27017/products?authSource=admin');
-
-const Product = mongoose.model('Product', { name: String, price: Number });
-app.get('/', (req, res) => {
-    return res.send('Welcome to Node js, express js in Docker');
+app.get("/", (req, res) => {
+    res.send("Hello Express");
 });
 
-app.post('/api/products', async (req, res) => {
-    const product = new Product({ name: req.body.name, price: req.body.price });
-    const savedProduct = await product.save();
-    return res.status(201).json(savedProduct);
+app.listen(PORT, () => {
+    console.log("ローカルサーバー起動中")
 });
-
-app.get('/api/products', async (req, res) => {
-    const products = await Product.find();
-    return res.json(products);
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-    const product = await Product.deleteOne({ id: req.params.id });
-    return res.json(product);
-});
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
